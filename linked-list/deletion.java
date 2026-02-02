fbb8c089ee44d15a9bc3b8c3e17f52aad1c0d8d0
Ritul-Ingole <ritulmi007@gmail.com>
2026-02-02 12:09:42 +0530

    Deletion: Removing the tail node

---
+++ linked-list/deletion.java
@@ -0,0 +1,42 @@
+class Node{
+  int data;
+  Node next;
+  Node(int val){
+    data = val;
+    next = null;
+  }
+}
+
+
+class Solution{
+  public Node deleteTail(Node head){
+    if(head == null || head.next == null) return null;
+
+    Node curr = head;
+    while(curr.next.next != null){
+      curr = curr.next;
+    }
+    curr.next = null;
+    return head;
+  }
+}
+
+public class deletion{
+  public static void main(String[] args){
+    
+    Node head = new Node(1);
+    head.next = new Node(2);
+    head.next.next = new Node(3);
+
+    Solution sol = new Solution();
+    head = sol.deleteTail(head);
+
+    System.out.println("After deletion List: ");
+
+    Node temp = head;
+    while(temp != null){
+      System.out.print(temp.data + " ");
+      temp = temp.next;
+    }
+  }
+}
